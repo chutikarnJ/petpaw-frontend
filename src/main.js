8153056bf95e64d7e216d8d4a28754b1d87a7f3b
marotate <6422782092@g.siit.tu.ac.th>
2025-06-03 20:38:47 +0700

    edit pinia persist import

--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import './index.css'
 import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persistedstate'
 import PrimeVue from 'primevue/config';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 
 
 const app = createApp(App)
 const pinia = createPinia()
-pinia.use(piniaPersist)
+pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
 app.use(PrimeVue);
