7def0ef48d5359b8222876ffbc0cf230c3ec7aa3
marotate <6422782092@g.siit.tu.ac.th>
2025-06-03 15:32:39 +0700

    edit api config

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:3001";
+
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: baseURL,
   withCredentials: true,
 });
 
